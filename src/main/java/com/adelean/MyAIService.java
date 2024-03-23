3d1addd08563fa0f10d188d01c621948965015ea
Benjamin Dauvissat <benjamin.dauvissat@adelean.com>
2024-03-23 19:08:18 +0100

    Initial commit

---
+++ src/main/java/com/adelean/MyAIService.java
@@ -0,0 +1,11 @@
+package com.adelean;
+
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+
+@RegisterAiService
+public interface MyAIService {
+
+    public String chat(@UserMessage String message);
+
+}
